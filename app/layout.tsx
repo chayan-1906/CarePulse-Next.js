686a4f44461b4362db3555d38a1819ec04491dcc
pdas9647 <padmanabhadas9647@gmail.com>
2025-01-07 14:31:00 +0530

    Done with Setup

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type {Metadata} from "next";
+import {Plus_Jakarta_Sans as FontSans} from "next/font/google";
+import "./globals.css";
+import React from "react";
+import {cn} from "@/lib/utils";
+import {ThemeProvider} from "@/components/theme-provider"
+
+const fontSans = FontSans({
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700"],
+    variable: "--font-sans",
+});
+
+export const metadata: Metadata = {
+    title: 'CarePulse',
+    description: 'A healthcare management system',
+    icons: {
+        icon: '/assets/icons/logo-icon.svg',
+    },
+}
+
+function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
+    return (
+        <html lang={'en'} suppressHydrationWarning>
+        <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
+        <ThemeProvider attribute={'class'} defaultTheme={'dark'}>
+            {children}
+        </ThemeProvider>
+        </body>
+        </html>
+    );
+}
+
+export default RootLayout;
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button";
+
+function Home() {
+    return (
+        <div className={'text-3xl text-purple-300 font-black underline'}>
+            <h1>Home</h1>
+            <Button>Click Me</Button>
+        </div>
+    );
+}
+
+export default Home;
